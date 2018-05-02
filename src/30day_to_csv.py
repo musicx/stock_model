5ed653b99024d26b335e480068e0b16957ec2f38
yijie.liu <yijie.liu@ele.me>
2018-05-02 17:26:06 +0800

    try dense model

--- src/30day_to_csv.py
+++ src/30day_to_csv.py
@@ -12,13 +12,15 @@ train = []
 test = []
 for stock in stocks.code.tolist():
     try:
-        candles = qa.QA_fetch_stock_day_adv(stock, start='2014-01-01', end='2018-04-25')
+        candles = qa.QA_fetch_stock_day_adv(stock, start='2016-01-01', end='2018-04-25')
         cdata = candles.data.reset_index(level=1, drop=True).loc[:, pvars]
         data = pd.concat([cdata - cdata.diff(x) for x in range(periods)], axis=1).apply(lambda x: x / cdata.close) - 1
         cdata2 = candles.data.reset_index(level=1, drop=True).loc[:, vvars]
         data2 = pd.concat([cdata2 - cdata2.diff(x) for x in range(periods)], axis=1).apply(lambda x: x / cdata2.vol)
         change = (cdata - cdata.diff(-3)).close / (cdata - cdata.diff(-1)).open - 1
-        label = (change > 0.1) * 1
+        # label = pd.concat([(change > 0.1), (change <= 0.1) & (change > 0),
+        #                    (change <= 0) & (change > -0.1), (change <= -0.1)], axis=1) * 1
+        label = (change > 0.1) * 1 + (change > 0) * 1 + (change > -0.1) * 1
 
         all_data = pd.concat([data, data2, label], axis=1).dropna()
         print('stock {} have {} examples, left {} examples'.format(stock, data.shape[0], all_data.shape[0]))
@@ -38,5 +40,5 @@ for stock in stocks.code.tolist():
 train = pd.concat(train, axis=0).reset_index()
 test = pd.concat(test, axis=0).reset_index()
 
-train.to_csv('train_14_17.csv', index=False)
-test.to_csv('test_14_17.csv', index=False)
+train.to_csv('../data/train_16_17.csv', index=False)
+test.to_csv('../data/test_16_17.csv', index=False)
