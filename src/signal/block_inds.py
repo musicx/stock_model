414ba9af078fe2fe8b8a9181a07f50f81fac3c7d
yijie.liu <yijie.liu@ele.me>
2019-01-15 21:30:37 +0800

    block code

--- src/signal/block_inds.py
+++ src/signal/block_inds.py
@@ -3,6 +3,14 @@ import QUANTAXIS as qa
 import datetime as dt
 
 
+def up_stage(data, col):
+    rate = data[col] / (data[col].shift(1)) - 1
+    six = (rate > 0.06) * 1
+    eight = (rate > 0.08) * 1
+    ten = (rate > 0.097) * 1
+    return pd.DataFrame({'cat': six+eight+ten, 'rate': rate})
+
+
 if __name__ == '__main__':
     today = dt.datetime.today()
     # today = dt.datetime(2018, 7, 6)
@@ -25,3 +33,6 @@ if __name__ == '__main__':
         block_names = block.data.reset_index().loc[:, ['code', 'blockname']]
         blocks.append(block_names)
     block_name = pd.concat(blocks, axis=0)
+
+    candles = qa.QA_fetch_stock_day_adv(stock_list, start='2019-01-01', end='2019-01-14').to_qfq()
+    stage = candles.add_func(up_stage, 'close')
