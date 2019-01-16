7e6e4a4e0f4232905a8fb5a86b8108a1d7d382a0
yijie.liu <yijie.liu@ele.me>
2019-01-16 20:38:21 +0800

    block code

--- src/signal/04_woe.py
+++ src/signal/04_woe.py
@@ -12,7 +12,7 @@ logging.basicConfig(format='%(asctime)-15s - %(name)s - %(levelname)s - %(messag
 file_names = os.listdir('../data/inds/')
 train = []
 test = []
-for name in file_names:
+for name in file_names[:2]:
     train_sub = pd.read_hdf('../data/inds/' + name, 'train')
     test_sub = pd.read_hdf('../data/inds/' + name, 'test')
     train.append(train_sub)
@@ -22,9 +22,11 @@ test_data = pd.concat(test, axis=0).fillna(0).reset_index(drop=True)
 train_data.drop(columns=['date'], inplace=True)
 test_data.drop(columns=['date'], inplace=True)
 
-splitter = BinSplitter(bad_name='go_up',
+splitter = BinSplitter(bad_name='go_up', # variables_to_check=['mtm_3_16_delta', 'mtmma_3_12_delta'],
                        min_observation=3, min_target_observation=0,
                        min_proportion=0, min_target_proportion=0, num_jobs=2)
+print(splitter.precheck_data(train_data))
+
 splitter.fit(train_data)
 splitter.apply(test_data)
 mrgr = BinMerger(min_drop=0, min_merge=0, min_merge_bad=0,
