b0575969342627d4eea62267bfd1bd154b1682d9
yijie.liu <yijie.liu@ele.me>
2019-04-29 19:37:24 +0800

    xgb model

--- src/signal/03_train_sth.py
+++ src/signal/03_train_sth.py
@@ -2,19 +2,34 @@ import numpy as np
 import pandas as pd
 import time
 # import QUANTAXIS as qa
-from sklearn.tree import DecisionTreeClassifier
+# from sklearn.tree import DecisionTreeClassifier
+import os
+import xgboost as xgb
 
 if __name__ == '__main__':
-    train = pd.read_hdf('../data/inds/600000.hdf', key='train')
 
-    tree = DecisionTreeClassifier(max_depth=20, min_samples_split=4, min_samples_leaf=2, random_state=33)
-    train_features = [x for x in train.columns if x not in {'date', 'code', 'go_up'}]
-    print(time.time())
-    tree.fit(train.fillna(0).loc[:, train_features], train.loc[:, 'go_up'].fillna(0))
-    print(time.time())
-    train['pred'] = [x[1] for x in tree.predict_proba(train.fillna(0).loc[:, train_features])]
-    print(train.groupby(['go_up', 'pred'])['code'].count())
+    file_names = os.listdir('../data/inds/')
+    train_files = []
+    test_files = []
+    for name in file_names:
+        if name.endswith('.hdf'):
+            train_sub = pd.read_hdf('../data/inds/' + name, 'train')
+            test_sub = pd.read_hdf('../data/inds/' + name, 'test')
+            train_files.append(train_sub)
+            test_files.append(test_sub)
+    train_data = pd.concat(train_files, axis=0).fillna(0).reset_index(drop=True)
+    test_data = pd.concat(test_files, axis=0).fillna(0).reset_index(drop=True)
+    train_data.drop(columns=['date'], inplace=True)
+    test_data.drop(columns=['date'], inplace=True)
 
-    test = pd.read_hdf('../data/inds/600000.hdf', key='test')
-    test['pred'] = [x[1] for x in tree.predict_proba(test.fillna(0).loc[:, train_features])]
-    print(test.groupby(['go_up', 'pred'])['code'].count())
+    model = xgb.XGBClassifier(max_depth=7, learning_rate=0.1, n_estimators=50, random_state=77)
+    # tree = DecisionTreeClassifier(max_depth=20, min_samples_split=4, min_samples_leaf=2, random_state=33)
+    train_features = [x for x in train_data.columns if x not in {'date', 'code', 'go_up'}]
+    print(time.time())
+    model.fit(train_data.loc[:, train_features], train_data.loc[:, 'go_up'])
+    print(time.time())
+    train_data['pred'] = [x[1] for x in model.predict_proba(train_data.loc[:, train_features])]
+    print(train_data.groupby(['go_up', 'pred'])['code'].count())
+
+    test_data['pred'] = [x[1] for x in model.predict_proba(test_data.loc[:, train_features])]
+    print(test_data.groupby(['go_up', 'pred'])['code'].count())
